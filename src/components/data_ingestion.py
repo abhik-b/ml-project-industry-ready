fc0f69b2a89adccb2d55a26bee7ac5babdd4e766
abhik-b <abhik0301@gmail.com>
2024-11-14 21:36:04 +0530

    data transformation done 🥳

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from src.logger import logging
 import pandas as pd
 import os
 import sys
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 
 @dataclass
@@ -54,4 +55,7 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
